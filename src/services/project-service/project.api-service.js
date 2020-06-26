054ff9fb5d337235becd00d59bf05b6b09d79d91
jjmedinamartin <jjmm89@gmail.com>
2020-06-26 13:50:34 +0200

    small refactor

--- src/services/project-service/project.api-service.js
+++ src/services/project-service/project.api-service.js
@@ -1,11 +1,9 @@
-import { service_string } from "../../settings/service-settings";
+import { getServiceString, ALL_PROJECTS_ENDPOINT } from "../../settings/service-settings";
 import { Project } from "./../../models/project-model";
-
-const AllProjectsEndpoint = "/projects";
 
 export const fetchAllProjects = () => {
   return Promise.resolve(
-    fetch(`${service_string()}${AllProjectsEndpoint}`)
+    fetch(getServiceString(ALL_PROJECTS_ENDPOINT))
       .then((response) => response.json())
       .then((api_projects) => {
         return api_projects.data.map(function (project_it) {
--- src/settings/service-settings.js
+++ src/settings/service-settings.js
@@ -1,6 +1,8 @@
 export const server_url = "http://localhost:8000";
 export const service_api = "/api";
 
-export const service_string = () => {
-  return `${server_url}${service_api}`;
+export const ALL_PROJECTS_ENDPOINT = "/projects";
+
+export const getServiceString = (endpoint) => {
+  return `${server_url}${service_api}${endpoint}`;
 };
