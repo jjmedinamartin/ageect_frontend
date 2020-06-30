5fafbab4a0cc62f2b9b41f94d6eee898e0438eef
jjmedinamartin <jjmm89@gmail.com>
2020-06-30 19:54:13 +0200

    first colors adjusts

--- src/components/landing-page/landing-page.component.js
+++ src/components/landing-page/landing-page.component.js
@@ -1,9 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
+import './landing-page.component.scss';
 
 export const LandingPageComponent = (props) => {
   const { projects } = props;
-return <div>{projects.length} CONTEXT {process.env.REACT_APP_CONTEXT}</div>;
+  return (
+    <div className="mainLayout">
+      {projects.length} CONTEXT {process.env.REACT_APP_CONTEXT}
+    </div>
+  );
 };
 
 LandingPageComponent.propTypes = {
